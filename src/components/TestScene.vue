33bc75fc862bcd4089e4e4bb351cccb260a7380c
ao10 <ao10@umbc.edu>
2019-02-07 16:02:37 -0500

    Added texture to sky and boxes. Boxes on each 4 corner of planes. Setting up tours soon.

--- src/components/TestScene.vue
+++ src/components/TestScene.vue
@@ -1,9 +1,21 @@
 <template>
   <a-scene stats>
-    <a-sky color="#8ddfff"></a-sky>
+    <a-assets>
+      <img id="boxTexture" src="https://i.imgur.com/mYmmbrp.jpg">
+      <img id="skyTexture" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg">
+    </a-assets>
+    <a-sky src="#skyTexture"></a-sky>
     <!-- https://aframe.io/docs/0.8.0/guides/building-a-basic-scene.html#placing-our-box-in-front-of-the-camera -->
-    <a-box color="red" position="0 0 -5" ></a-box>
-    <a-plane color="#0F2" height="20" width="10" rotation="-90 0 0"></a-plane>
+    <!-- position = x y z -->
+    <!-- X is LEFT/RIGHT -->
+    <!-- Y is UP/DOWN -->
+    <!-- Z is TOWARDS/AWAY -->
+    <a-box src="#boxTexture" position="-4.4 0.5 -8" ></a-box>
+    <a-box src="#boxTexture" position="4.4 0.5 -8" ></a-box>
+    <a-box src="#boxTexture" position="-4.4 0.5 8" ></a-box>
+    <a-box src="#boxTexture" position="4.4 0.5 8" ></a-box>
+    
+    <a-plane color="#0F2" height="20" width="10" transparent=true rotation="-90 0 0"></a-plane>
   </a-scene>
 </template>
 
